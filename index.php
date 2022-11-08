f9346123cd9313b0b41546001cd0cf9ee2bd195b
Tifaine RIVOIRE <t.rivoire@optisecurite.fr>
2022-11-08 11:07:20 +0100

    lorem ipsum

---
+++ index.php
@@ -0,0 +1,47 @@
+<!--##########################################################
+#                                                            #
+#                                                            #
+#                                                            #
+#                  ________ .__.____                         #
+#                  \_____  \|__|    |                        #
+#                    _(__  <|  |    |                        #
+#                   /       \  |    |___                     #
+#                  /______  /__|_______ \                    #
+#                         \/           \/                    #
+#                                                            #
+###########################################################-->
+<!DOCTYPE html>
+<html>
+    <header> <!-- A ne pas confondre avec <head> -->
+        <h1>3iSystem</h1>
+        <p>
+            <a href="https://www.3il-ingenieurs.fr/" target="_blank">3iL Academy</a>
+        </p>
+    </header>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="CSS/styles.css">
+        <title>3iSystem</title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+            <li><a href = "services.pdf" target = "_blanck">Services</a></li>
+            <li><a href = "tarifs.html">Tarifs</a></li>
+            <li><a href="mailto:t.rivoire@optisecurite.fr">Contact</a></li>
+            </ul></nav>
+    <p><i>Vous souhaitez migrer vers les solutions Cloud ?</i></p>
+    <p>3iSystem est là pour booster l'industrialisation de vos process.</p>
+
+    <h2>Cloud</h2>
+    <p>Sécurisez le patrimoine numérique de votre entreprise en stockant vos données sur nos infrastructures sécurisées et offrez-vous un accès sécurisé en tout lieu.</p>
+    
+    <h2>Cybersécurité/Sauvegarde</h2>
+    <p>Laissez notre équipe d'experts en solutions de cyber-sécurité gérer vos services de sécurité.</p>
+    
+    <h2>Maintenance Hotline</h2>
+    <p>Depuis plus de 15 ans, des experts répondent à vos questions pour résoudre vos problèmes.</p>
+    
+</body>
+<footer>©3iSystem2022</footer>
+</html>
